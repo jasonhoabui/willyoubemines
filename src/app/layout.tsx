844ebe5ade4a284b8b4aa99449d23ccaaf4fa195
Jason Bui <jasonbui@169-231-74-100.wireless.ucsb.edu>
2025-02-10 01:07:19 -0800

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Head from 'next/head';
 import ClientWrapper from './ClientWrapper';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
