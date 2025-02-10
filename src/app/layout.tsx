89d5ef3e0274a0a49829d9f05468a29bd3073d6e
Jason Bui <jasonbui@169-231-74-100.wireless.ucsb.edu>
2025-02-10 01:31:52 -0800

    added name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
-import ClientWrapper from './ClientWrapper';
-import { Analytics } from '@vercel/analytics/react';
+import Head from "next/head";
+import ClientWrapper from "./ClientWrapper";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,10 +32,17 @@ export default function RootLayout({
       </Head>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
         >
           <ClientWrapper>{children}</ClientWrapper>
           <Analytics />
+          <div className="w-full flex justify-center mt-auto pb-6">
+            <footer className="text-center">
+              <p className="text-gray-500 text-sm">
+                ©{new Date().getFullYear()} by Jason Bui
+              </p>
+            </footer>
+          </div>
         </body>
       </html>
     </>
